b07609232f36c6722e117f24ee32d7342a604a48
rifqi2242 <rifqimuzakki45@gmail.com>
2024-10-08 22:11:22 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,118 +1,171 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  FlatList,
 } from 'react-native';
+import React, {useState} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
+interface toDo {
+  id: number;
   title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  statue: boolean;
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [title, setTitle] = useState<string>('');
+  const [toDo, setToDo] = useState<toDo[]>([]);
+  const [editId, setEditId] = useState<number>(0);
+  const [editTitle, setEditTitle] = useState<string>('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleAdd = () => {
+    if (title.trim() === '') {
+      return;
+    }
+
+    const newToDo = {
+      id: Date.now(),
+      title: title,
+      statue: false,
+    };
+
+    setToDo(prev => [...prev, newToDo]);
+
+    setTitle('');
+  };
+
+  const handleDelete = (deleteId: number) => {
+    if (deleteId != 0) {
+      const updateToDo = toDo.filter(item => item.id !== deleteId);
+      setToDo(updateToDo);
+    }
+    return;
+  };
+
+  const handleEdit = () => {
+    if (editTitle.trim() === '') {
+      return;
+    } else {
+      setToDo(prev =>
+        prev.map(item =>
+          item.id === editId ? {...item, title: editTitle} : item,
+        ),
+      );
+
+      setEditId(0);
+      setEditTitle('');
+    }
+  };
+
+  const handleStartEdit = (editId: number, currentTitle: string) => {
+    setEditId(editId);
+    setEditTitle(currentTitle);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+    <View style={style.container}>
+      <View style={style.row}>
+        <TextInput
+          style={style.input}
+          placeholder="Coba ges"
+          value={title}
+          onChangeText={setTitle}
+        />
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+        <Pressable style={style.button} onPress={handleAdd}>
+          <Text style={style.texts}>Add</Text>
+        </Pressable>
+      </View>
+
+      <FlatList
+        data={toDo}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <View style={style.flex}> 
+            <View style={[style.row, style.bt]}>
+              {item.id === editId ? (
+                <TextInput
+                  style={style.editInput}
+                  value={editTitle}
+                  onChangeText={setEditTitle}
+                />
+              ) : (
+                <Text style={style.texts}>{item.title}</Text>
+              )}
+              <View style={style.row}>
+                {item.id === editId ? (
+                  <Pressable
+                    style={buttonRadius('green').buttonRad}
+                    onPress={handleEdit}>
+                    <Text style={style.texts}>Save</Text>
+                  </Pressable>
+                ) : (
+                  <Pressable
+                    style={buttonRadius('blue').buttonRad}
+                    onPress={() => handleStartEdit(item.id, item.title)}>
+                    <Text style={style.texts}>Edit</Text>
+                  </Pressable>
+                )}
+                <Pressable
+                  style={buttonRadius('red').buttonRad}
+                  onPress={() => handleDelete(item.id)}>
+                  <Text style={style.texts}>Delete</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  flex: {
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
+  texts: {
+    color: 'black',
     fontSize: 18,
-    fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    borderColor: 'black',
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  button: {
+    paddingHorizontal: 20,
+    backgroundColor: 'blue',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  bt: {
+    justifyContent: 'space-between',
+  },
+  editInput: {
+    borderColor: 'black',
+    color: 'black',
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 8,
+    borderWidth: 1,
   },
 });
+
+const buttonRadius = (color: any) =>
+  StyleSheet.create({
+    buttonRad: {
+      padding: 20,
+      backgroundColor: color,
+      borderRadius: 12,
+    },
+  });
 
 export default App;
